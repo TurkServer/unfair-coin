eb2607b2904a1d15b3e4929de73d06e2b2fd30b3
Xi Alice Gao <alicexigao@gmail.com>
2016-09-26 15:23:46 -0700

    More tutorial pages and instructions.

--- client/tutorial.js
+++ client/tutorial.js
@@ -1,10 +1,22 @@
 var tutorialSteps = [
-  { 
-    template: Template.tut_biased_coin
-  },
-  {
-    template: Template.tut_goal
-  }
+{
+  template: Template.tut_player
+},
+{ 
+  template: Template.tut_biased_coin
+},
+{
+  template: Template.tut_goal
+},
+{
+  template: Template.tut_info
+},
+{
+  template: Template.tut_guess
+},
+{
+  template: Template.tut_payment
+}
 ];  
 
 Template.woc_tutorial.helpers({
@@ -12,6 +24,27 @@ Template.woc_tutorial.helpers({
     steps: tutorialSteps,
     onFinish: function() {
       Router.go('experiment');
+    }
+  },
+  treatmentInst: function() {
+    const game = Games.findOne();
+    if (game == null) {
+      console.log("Null game");
+      return null;
+    }
+
+    if (game.incentive === "ind") {
+      return "instIndiv";
+    }
+    else if (game.incentive === "comp") {
+      return "instComp";
+    }
+    else if (game.incentive === "coll") {
+      return "instColl";
+    }
+    else {
+      console.log("Unknown incentive");
+      return null;
     }
   }  
 });
