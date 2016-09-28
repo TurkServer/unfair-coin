d1f65b368af2f050d40e7df9714c3fffe1cc6ef8
Andrew Mao <mizzao@gmail.com>
2016-09-28 17:43:57 -0400

    example of integrating tutorial with interface

--- client/tutorial.js
+++ client/tutorial.js
@@ -1,29 +1,38 @@
 var tutorialSteps = [
-{
-  template: Template.tut_player
-},
-{ 
-  template: Template.tut_biased_coin
-},
-{
-  template: Template.tut_goal
-},
-{
-  template: Template.tut_info
-},
-{
-  template: Template.tut_communication
-},
-{
-  template: Template.tut_payment
-}
-];  
+  {
+    template: Template.tut_overview
+  },
+  {
+    template: Template.tut_player
+  },
+  {
+    template: Template.tut_biased_coin
+  },
+  {
+    template: Template.tut_goal,
+    spot: ".flip-guesser"
+  },
+  {
+    template: Template.tut_info,
+    spot: ".coin-flips"
+  },
+  {
+    template: Template.tut_communication
+  },
+  {
+    template: Template.tut_payment
+  }
+];
 
-Template.woc_tutorial.helpers({
-  options: {
-    steps: tutorialSteps,
-    onFinish: function() {
-      Router.go('experiment');
-    }
-  } 
+Session.setDefault("tutorialEnabled", true);
+
+Template.registerHelper("tutorialOptions", {
+  steps: tutorialSteps,
+  onFinish: function() {
+    Session.set("tutorialEnabled", false);
+  }
+});
+
+Template.registerHelper("tutorialEnabled", function() {
+  return Session.equals("tutorialEnabled", true);
 });
